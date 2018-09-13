932d36ae60a1d19a15f0ddb02343aedb298d0e26
Roger-luo <hiroger@qq.com>
2018-09-13 11:24:57 -0400

    add test for scalar binary op

--- tests/test_tensor.py
+++ tests/test_tensor.py
@@ -10,6 +10,20 @@ class TestComplexTensor(unittest.TestCase):
     def test_indexing(self):
         t = torch.empty(2, 2, dtype=torch.complex128)
         t[1]
+        t[1, 1]
+
+    def test_fill(self):
+        t = torch.empty(2, 2, dtype=torch.complex128)
+        t.fill_(1.0)
+        t.fill_(1.0 + 2.0j)
+    
+    def test_scalar_binary_op(self):
+        a = torch.ones(2, 2, dtype=torch.complex128)
+        2 * a
+        2 / a
+        2 - a
+        2 + a
+
 
 
 if __name__ == '__main__':
